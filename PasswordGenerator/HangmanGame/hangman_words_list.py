6d0044230c2fb043eb22b501f01e2d8a2a7e21fb
Ankush Dhingra <ankushkumar20024@gmail.com>
2025-03-17 22:38:40 +0530

    add word list in hangman_words_list.py file

---
+++ PasswordGenerator/HangmanGame/hangman_words_list.py
@@ -0,0 +1,12 @@
+word_list = [
+    "apple", "banana", "grape", "orange", "mango", "papaya", "pineapple", "watermelon", "guava", "kiwi",
+    "strawberry", "blueberry", "raspberry", "blackberry", "cherry", "peach", "plum", "pear", "apricot", "fig",
+    "pomegranate", "lychee", "coconut", "jackfruit", "durian", "rambutan", "mangosteen", "tamarind", "custardapple", "sapota",
+    "dragonfruit", "passionfruit", "starfruit", "avocado", "grapefruit", "lemon", "lime", "tangerine", "mandarin", "clementine",
+    "date", "olive", "persimmon", "quince", "mulberry", "elderberry", "gooseberry", "cranberry", "boysenberry", "huckleberry",
+    "nectarine", "soursop", "longan", "santol", "langsat", "pulasan", "salak", "marang", "sugarapple", "breadfruit",
+    "bilimbi", "carambola", "jabuticaba", "miraclefruit", "noni", "pitanga", "surinamcherry", "ugli", "yumberry", "ziziphus",
+    "amla", "bael", "ber", "chikoo", "jamun", "karonda", "kokum", "lasoda", "mahua", "phalsa",
+    "tendu", "woodapple", "ziziphus", "bittergourd", "bottle gourd", "brinjal", "cabbage", "carrot", "cauliflower", "cucumber",
+    "drumstick", "eggplant", "fenugreek", "garlic", "ginger", "greenchilli", "ivy gourd", "ladyfinger", "mint", "onion"
+]
