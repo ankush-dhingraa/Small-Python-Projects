947a1aab03cea481ebf0b60e6dd4f331bd285140
Ankush Dhingra <ankushkumar20024@gmail.com>
2025-03-19 00:26:29 +0530

    finale touches

--- Caesar Cipher/CaesarCipher.py
+++ Caesar Cipher/CaesarCipher.py
@@ -1,3 +1,6 @@
+import logo
+print(logo.logo)
+print("***************************[START]***************************")
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 def encrypt(original_text,shift):
     encrypted_text = ""
@@ -6,7 +9,7 @@ def encrypt(original_text,shift):
             encrypted_text += " "
         else:
             encrypted_text += alphabet[(alphabet.index(letters)-len(alphabet))+shift]
-    print(f"Encrypted text is : {encrypted_text}")
+    print(f"Encrypted text is : {encrypted_text}\n")
 
 def decrypt(original_text,shift):
     decryted_text = ""
@@ -15,11 +18,24 @@ def decrypt(original_text,shift):
             decryted_text += " "
         else:
             decryted_text += alphabet[alphabet.index(letter)-shift]
-    print(f"Decrypted text is : {decryted_text}")
+    print(f"Decrypted text is : {decryted_text}\n")
+def caesar(original_text, shift, encode_or_decode):
+    if encode_or_decode.lower() == "encode":
+        encrypt(original_text,shift)
+    elif encode_or_decode.lower() == "decode":
+        decrypt(original_text,shift)
+    else:
+        print("Wrong choice, do agian")
 
-text = input("Enter the text to encrypt : ").lower()
-shift = int(input("Enter the shift value : "))
-encrypt(text,shift)
-text = input("Enter the text to decrypt : ").lower()
-shift = int(input("Enter the shift value : "))
-decrypt(text,shift)
+stop = False
+while not stop:
+    encode_or_decode = input("Enter 'encode' to encrypt or 'decode' to decrypt : ")
+    text = input(f"Enter the text to {encode_or_decode} : ").lower()
+    shift = int(input("Enter the shift value : "))
+    caesar(text,shift,encode_or_decode)
+    choice = input("Enter 'yes' to try again, or 'no' to exit : ").lower()
+    if choice == "yes":
+        stop = False
+    elif choice == "no":
+        stop = True
+        print("***************************[EXIT]***************************")
