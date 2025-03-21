8f473393d0d3abc5d35d9ed12347216a9150fc12
Ankush Dhingra <ankushkumar20024@gmail.com>
2025-03-21 21:25:18 +0530

    on going

--- The Blackjack Capstone Project/main.py
+++ The Blackjack Capstone Project/main.py
@@ -5,6 +5,7 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 user_cards = []
 computer_cards = []
 should_play = True
+taking_card = True
 #for user 
 def give_cards_to_user(num = 1):
     for i in range(num):
@@ -12,11 +13,33 @@ def give_cards_to_user(num = 1):
 #for computer
 def give_cards_to_computer(num = 1):
     for i in range(num):
-        user_cards.append(random.choice(cards))
+        computer_cards.append(random.choice(cards))
+
+def info_print():
+    print(f"Your cards: {user_cards}, current score: {sum(user_cards)}\nComputer's first card: {computer_cards}")
+    
+def check_status():
+    if sum(user_cards) == 21:
+
+    
+
+
+
+
 while should_play:
     choice_for_play = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
     if choice_for_play == "y":
         give_cards_to_user(2)
-        
+        give_cards_to_computer()
+        info_print()
+        check_status()
+        while taking_card:
+            user_input = input("Type 'Hit' to get another card, type 'stand' to pass: ").lower()
+            if user_input == "hit":
+                give_cards_to_user()
+            else:
+                taking_card = False
+
+        check_status()
     else:
         should_play = False
