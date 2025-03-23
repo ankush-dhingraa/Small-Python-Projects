6842b703b8cd48b3af9db159761dc4c6167b3a46
Ankush Dhingra <ankushkumar20024@gmail.com>
2025-03-23 18:34:04 +0530

    work in progress

--- Number Guessing Game/main.py
+++ Number Guessing Game/main.py
@@ -1,3 +1,7 @@
 from ascii_art import logo,winner
-print(logo)
-winner()
+game_over = False
+live = 0
+while not game_over:
+    print(logo)
+    print("*********************************[ START ]*********************************")
+    print("")
