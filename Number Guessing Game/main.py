5beb44556ae5a043a197b130344b61699af495c2
Ankush Dhingra <ankushkumar20024@gmail.com>
2025-03-23 18:41:13 +0530

    work in progress adding a loops

--- Number Guessing Game/main.py
+++ Number Guessing Game/main.py
@@ -1,7 +1,19 @@
 from ascii_art import logo,winner
 game_over = False
-live = 0
+lives = 0
+def check():
+
 while not game_over:
     print(logo)
     print("*********************************[ START ]*********************************")
-    print("")
+    print("Welcome to the Guess the Number Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    level = input("Select a difficulty level: Type 'easy' or 'hard' : ")
+    if level == "easy":
+        lives = 10
+    else:
+        lives = 5
+    
+    while lives >0:
+        guess = int(input)
+        
