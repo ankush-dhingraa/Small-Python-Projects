57b6f8757794eace0cd28c9bda3f1de31d4762c5
Ankush Dhingra <ankushkumar20024@gmail.com>
2025-03-24 16:53:51 +0530

    work on progress

--- Higher or Lower Game on  Instagram Followers/game_data.py
+++ Higher or Lower Game on  Instagram Followers/game_data.py
@@ -3,4 +3,10 @@ data = [{
     "follower_count":400,
     "description":"official account",
     "country":"USA"
+        },
+        {
+    "name":"instagram",
+    "follower_count":400,
+    "description":"official account",
+    "country":"USA"
         }]
