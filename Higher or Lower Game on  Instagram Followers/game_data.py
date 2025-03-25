34a296376648d8c5c39b887cce518d8b0e980f64
Ankush Dhingra <ankushkumar20024@gmail.com>
2025-03-25 20:20:32 +0530

    adding data for game

--- Higher or Lower Game on  Instagram Followers/game_data.py
+++ Higher or Lower Game on  Instagram Followers/game_data.py
@@ -11,3 +11,227 @@ data = [{
     "description":"official account",
     "country":"USA"
         }]
+
+data = [
+    {
+        "name": "Virat Kohli",
+        "follower_count": 270,
+        "description": "Indian cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Priyanka Chopra",
+        "follower_count": 92.5,
+        "description": "Actress and singer",
+        "country": "India"
+    },
+    {
+        "name": "Shraddha Kapoor",
+        "follower_count": 94.2,
+        "description": "Bollywood actress",
+        "country": "India"
+    },
+    {
+        "name": "Alia Bhatt",
+        "follower_count": 86.2,
+        "description": "Bollywood actress",
+        "country": "India"
+    },
+    {
+        "name": "Narendra Modi",
+        "follower_count": 92.4,
+        "description": "Prime Minister of India",
+        "country": "India"
+    },
+    {
+        "name": "Katrina Kaif",
+        "follower_count": 80.4,
+        "description": "Bollywood actress",
+        "country": "India"
+    },
+    {
+        "name": "Deepika Padukone",
+        "follower_count": 80.4,
+        "description": "Bollywood actress",
+        "country": "India"
+    },
+    {
+        "name": "Neha Kakkar",
+        "follower_count": 78.4,
+        "description": "Indian singer",
+        "country": "India"
+    },
+    {
+        "name": "Urvashi Rautela",
+        "follower_count": 72.5,
+        "description": "Actress and model",
+        "country": "India"
+    },
+    {
+        "name": "Jacqueline Fernandez",
+        "follower_count": 71.1,
+        "description": "Actress and model",
+        "country": "India"
+    },
+    {
+        "name": "Akshay Kumar",
+        "follower_count": 66.2,
+        "description": "Bollywood actor",
+        "country": "India"
+    },
+    {
+        "name": "Salman Khan",
+        "follower_count": 66.1,
+        "description": "Bollywood actor",
+        "country": "India"
+    },
+    {
+        "name": "Ayeza Khan",
+        "follower_count": 13.9,
+        "description": "Pakistani actress and model",
+        "country": "Pakistan"
+    },
+    {
+        "name": "Hania Aamir",
+        "follower_count": 13.7,
+        "description": "Pakistani actress",
+        "country": "Pakistan"
+    },
+    {
+        "name": "Aiman Khan",
+        "follower_count": 12.1,
+        "description": "Pakistani actress",
+        "country": "Pakistan"
+    },
+    {
+        "name": "Sarah Khan",
+        "follower_count": 11.8,
+        "description": "Pakistani actress",
+        "country": "Pakistan"
+    },
+    {
+        "name": "Iqra Aziz",
+        "follower_count": 11.1,
+        "description": "Pakistani actress",
+        "country": "Pakistan"
+    },
+    {
+        "name": "Mahira Khan",
+        "follower_count": 11,
+        "description": "Pakistani actress",
+        "country": "Pakistan"
+    },
+    {
+        "name": "Sajal Ali",
+        "follower_count": 10.3,
+        "description": "Pakistani actress and model",
+        "country": "Pakistan"
+    },
+    {
+        "name": "Minal Khan",
+        "follower_count": 10.2,
+        "description": "Pakistani actress",
+        "country": "Pakistan"
+    },
+    {
+        "name": "Imran Khan",
+        "follower_count": 9.5,
+        "description": "Politician and former cricket captain",
+        "country": "Pakistan"
+    },
+    {
+        "name": "Kinza Hashmi",
+        "follower_count": 9.2,
+        "description": "Pakistani actress",
+        "country": "Pakistan"
+    },
+    {
+        "name": "Sana Javed",
+        "follower_count": 8.9,
+        "description": "Pakistani actress",
+        "country": "Pakistan"
+    },
+    {
+        "name": "Imran Abbas",
+        "follower_count": 8.6,
+        "description": "Actor and model",
+        "country": "Pakistan"
+    },
+    {
+        "name": "Mawra Hocane",
+        "follower_count": 8.5,
+        "description": "Actress and model",
+        "country": "Pakistan"
+    },
+    {
+        "name": "Feroze Khan",
+        "follower_count": 8.4,
+        "description": "Actor and model",
+        "country": "Pakistan"
+    },
+    {
+        "name": "Maya Ali",
+        "follower_count": 8,
+        "description": "Pakistani actress",
+        "country": "Pakistan"
+    },
+    {
+        "name": "Selena Gomez",
+        "follower_count": 400,
+        "description": "Singer and actress",
+        "country": "USA"
+    },
+    {
+        "name": "Dwayne 'The Rock' Johnson",
+        "follower_count": 350,
+        "description": "Actor and former wrestler",
+        "country": "USA"
+    },
+    {
+        "name": "Kim Kardashian",
+        "follower_count": 340,
+        "description": "Media personality and businesswoman",
+        "country": "USA"
+    },
+    {
+        "name": "Ariana Grande",
+        "follower_count": 330,
+        "description": "Singer and actress",
+        "country": "USA"
+    },
+    {
+        "name": "Beyoncé",
+        "follower_count": 280,
+        "description": "Singer and actress",
+        "country": "USA"
+    },
+    {
+        "name": "Justin Bieber",
+        "follower_count": 270,
+        "description": "Singer",
+        "country": "USA"
+    },
+    {
+        "name": "Kylie Jenner",
+        "follower_count": 260,
+        "description": "Media personality and businesswoman",
+        "country": "USA"
+    },
+    {
+        "name": "Taylor Swift",
+        "follower_count": 240,
+        "description": "Singer-songwriter",
+        "country": "USA"
+    },
+    {
+        "name": "Kendall Jenner",
+        "follower_count": 230,
+        "description": "Model and media personality",
+        "country": "USA"
+    },
+    {
+        "name": "Lionel Messi",
+        "follower_count": 220,
+        "description": "Footballer",
+        "country": "Argentina"}]
+ 
