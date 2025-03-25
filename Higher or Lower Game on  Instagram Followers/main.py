baa1097012ea123c1f87d2d8eb6c260ab7e8ca1f
Ankush Dhingra <ankushkumar20024@gmail.com>
2025-03-25 20:30:48 +0530

    create function data_to_compare()

--- Higher or Lower Game on  Instagram Followers/main.py
+++ Higher or Lower Game on  Instagram Followers/main.py
@@ -2,3 +2,16 @@
 import random
 from ascii_art import logo,vs
 from game_data import data
+data_a = {}
+data_b = {}
+def data_to_compare():
+    global data_a,data_b
+    data_a = random.choice(data)
+    flag = True
+    while flag:
+        temp = random.choice(data)
+        if data_a != temp:
+            data_b = temp
+            flag = False
+data_to_compare()
+print(data_a,"\n",data_b)
