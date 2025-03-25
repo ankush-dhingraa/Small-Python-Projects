593209c359e9ace6ade4e95035e680588673c44e
Ankush Dhingra <ankushkumar20024@gmail.com>
2025-03-25 22:22:05 +0530

    work in progress

--- Higher or Lower Game on  Instagram Followers/main.py
+++ Higher or Lower Game on  Instagram Followers/main.py
@@ -5,12 +5,20 @@ from game_data import data
 data_a = {}
 data_b = {}
 score = 0
+def compare(data_a,data_b,choice):
+    global score
+    if data_a["follower_count"] > data_b["follower_count"]:
+        if choice == "a":
+            score += 1
+    elif data_a["follower_count"] < data_b["follower_count"]:
+        if choice == "b":
+            score += 1
 def display(item):
     account_name = item["name"]
     account_descr = item["description"]
     account_country = item["country"]
     print(f"{account_name}, a {account_descr}, from {account_country}")
-def data_to_compare():
+def initialize_data():
     global data_a,data_b
     data_a = random.choice(data)
     flag = True
@@ -19,6 +27,6 @@ def data_to_compare():
         if data_a != temp:
             data_b = temp
             flag = False
-data_to_compare()
+initialize_data()
 display(data_a)
 display(data_b)
