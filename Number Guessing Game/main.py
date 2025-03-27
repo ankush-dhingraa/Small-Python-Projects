abe56574867bd5748cb997e4854ee00c23243f7e
Ankush Dhingra <ankushkumar20024@gmail.com>
2025-03-27 19:29:42 +0530

    Completed project - ready for release

--- Number Guessing Game/main.py
+++ Number Guessing Game/main.py
@@ -23,7 +23,7 @@ def check(guess):
       
 while not game_over:
     print(logo)
-    print("*********************************[ START ]*********************************")
+    print("**********************************[ START ]**********************************")
     print("Welcome to the Guess the Number Game!")
     print("I'm thinking of a number between 1 and 100.")
     level = input("Select a difficulty level: Type 'easy' or 'hard' : ")
