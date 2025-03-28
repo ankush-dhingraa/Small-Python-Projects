cb62b22f11e9f966d312786e2d13d6d3d468fa99
ankush <ankushkumar20024@gmail.com>
2025-03-28 12:47:04 +0530

    Added coffee menu with ingredients and pricing

--- Coffee Machine/main.py
+++ Coffee Machine/main.py
@@ -10,6 +10,24 @@ MENU = {
         "milk" : 80,
         "coffee" : 50,
     },
-    "cost" : 100}
-
+    "cost" : 100},
+    "Hyderabadi Irani Coffee" : {"ingredients" : {
+        "Water" : 80,
+        "milk" : 100,
+        "coffee" : 40,
+    },
+    "cost" : 90}
 }
+print(MENU["Indian Espresso"]["ingredients"])
+
+for keys in MENU:
+    print(keys," : ")
+    for i in MENU[keys]:
+        if i == "ingredients":
+            for j in MENU[keys][i]:
+                print(j," : ",MENU[keys][i][j])
+        else:
+            print(i," : ",MENU[keys][i])
+    print("\n")
+        
+
