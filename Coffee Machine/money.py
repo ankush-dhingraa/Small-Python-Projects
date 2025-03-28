344bc8c43d73a4884b909f04d424a47e4514fb7a
ankush <ankushkumar20024@gmail.com>
2025-03-29 00:57:21 +0530

    create a new file money.py

---
+++ Coffee Machine/money.py
@@ -0,0 +1,14 @@
+ruppe_structure = '''
+0 for ₹1 coin
+1 for ₹2 coin
+2 for ₹5 coin
+3 for ₹10 Note
+4 for ₹20 Note
+5 for ₹50 Note
+6 for ₹100 Note
+7 for ₹200 Note
+8 for ₹500 Note
+'''
+ruppe_dict = {
+    
+}
