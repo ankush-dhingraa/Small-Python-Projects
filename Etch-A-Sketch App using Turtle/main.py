4132207e885d5be9f9e09787653cb69b6211cefb
ankush <ankushkumar20024@gmail.com>
2025-04-02 15:03:22 +0530

    Etch-A-Sketch App

--- Etch-A-Sketch App using Turtle/main.py
+++ Etch-A-Sketch App using Turtle/main.py
@@ -4,5 +4,6 @@ screen = Screen()
 # move_forward() is used for moving turtle forward by 10
 def move_forward():
     obj.forward(10)
+# move_backward() is used for moving turtle forward by 10
 def move_backward():
     obj.backward(10)
