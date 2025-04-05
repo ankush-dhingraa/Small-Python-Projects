9a45f78fce97f19edcb604151cadd98e5168fe6b
ankush <ankushkumar20024@gmail.com>
2025-04-05 21:29:43 +0530

    work in progress

--- Quiz Game Start/quiz_brain.py
+++ Quiz Game Start/quiz_brain.py
@@ -1,6 +1,7 @@
 class Quiz_brain:
     def __init__(self,q_list):
         self.question_no = 0
+        self.score = 0
         self.question_list = q_list
     
     def next_question(self):
@@ -14,5 +15,8 @@ class Quiz_brain:
     def check_answer(self,user_aswer,correct_answer):
         if user_aswer.lower() == correct_answer.lower():
             print("You got it right!")
+            self.score +=1
         else:
             print("That's wrong")
+        print(f"The correct answer was {correct_answer}")
+        print(f"Your current score is : {self.score}/{self.question_no}")
