42f040b9f1a589ebb1f094433de35a6263fa9bd8
ankush <ankushkumar20024@gmail.com>
2025-04-05 21:19:46 +0530

    work in progress

--- Quiz Game Start/quiz_brain.py
+++ Quiz Game Start/quiz_brain.py
@@ -6,8 +6,12 @@ class Quiz_brain:
     def next_question(self):
         current_question = self.question_list[self.question_no]
         self.question_no +=1
-        user = input(f"Q.{self.question_no} : {current_question.text}. (True/False)? :")
+        user_answer = input(f"Q.{self.question_no} : {current_question.text}. (True/False)? :")
     
     def stil_has_questions(self):
         return self.question_no < len(self.question_list)
-        
+    def check_answer(self,user_aswer,correct_answer):
+        if user_aswer.lower() == correct_answer.lower():
+            print("You got it Right!")
+        else:
+            print("That's wrong")
