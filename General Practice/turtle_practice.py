69f4c27389071646ca8eb933d376a21a120ac94a
ankush <ankushkumar20024@gmail.com>
2025-04-06 23:46:16 +0530

    Turtle Drawing Practice

--- General Practice/turtle_practice.py
+++ General Practice/turtle_practice.py
@@ -1,9 +1,13 @@
 from turtle import Turtle, Screen
 
 obj = Turtle()
-
+colours = ["red", "green", "black", "blue"]
 obj.shape("turtle")
-obj.color("green")
+for i in colours:
+    obj.color(i)
+    obj.forward(150)
+    obj.right(90)
+
 
 
 
