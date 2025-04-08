1183ad1dc03406818e2eaa130dba07f13f812787
ankush <ankushkumar20024@gmail.com>
2025-04-09 00:56:06 +0530

    work in progress

--- Turtle Race Game/main.py
+++ Turtle Race Game/main.py
@@ -1,4 +1,9 @@
 from turtle import Turtle, Screen
 obj = Turtle()
 screen = Screen()
+screen.setup(width=500,height=400)
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+screen.textinput(title="Make your bet")
+for color in colors:
+
 screen.exitonclick()
