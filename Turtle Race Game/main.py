524e8136f5051061e63a115be5d7c86f67775322
ankush <ankushkumar20024@gmail.com>
2025-04-09 00:58:42 +0530

    work in progress

--- Turtle Race Game/main.py
+++ Turtle Race Game/main.py
@@ -3,7 +3,9 @@ obj = Turtle()
 screen = Screen()
 screen.setup(width=500,height=400)
 colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
-screen.textinput(title="Make your bet")
+user_bet = screen.textinput(title="Make your bet",prompt="Which turtle will win the race? Enter color : ")
+print(user_bet)
 for color in colors:
+    pass
 
 screen.exitonclick()
