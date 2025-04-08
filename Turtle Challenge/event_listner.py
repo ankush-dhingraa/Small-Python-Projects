a54e1db40c6de992dfcbc1094daa6422d9fe5e8f
ankush <ankushkumar20024@gmail.com>
2025-04-08 23:55:04 +0530

    turtle graphics practice

--- Turtle Challenge/event_listner.py
+++ Turtle Challenge/event_listner.py
@@ -2,16 +2,8 @@ from turtle import Turtle, Screen
 obj = Turtle() 
 screen = Screen()
 
-def move_forwards():
+def move():
     obj.forward(10)
-def move_backwards():
-    obj.backward(10)
-def clockwise():
-    obj.right(10)
-def counter_clockwise():
-    obj.left(10)
-
 screen.listen()
-screen.onkey(key="w",fun=move_forwards)
-screen.onkey(key="s",fun=move_backwards)
+screen.onkey(key="space",fun=move)
 screen.exitonclick()
