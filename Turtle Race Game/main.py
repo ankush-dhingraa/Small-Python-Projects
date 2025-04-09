ced5f25bc96336e8950758b03e042ef3a0c25e53
ankush <ankushkumar20024@gmail.com>
2025-04-10 00:04:08 +0530

    Turtle race game is ready

--- Turtle Race Game/main.py
+++ Turtle Race Game/main.py
@@ -1,17 +1,29 @@
 from turtle import Turtle, Screen
 import random
 
-def race_winner(turtle_color):
-    if user_bet == turtle_color:
-        print("you win")
+def race_winner(turtle):
+    turtle_color = turtle.color()
+    if user_bet == turtle_color[0]:
+        print(f"You've won! The {turtle_color[0]} turtle is winner")
     else:
-        print("you lose")
+        print(f"You've lost! The {turtle_color[0]} turtle is winner")
+
+#end line 
+end_line = Turtle()
+end_line.penup()
+end_line.goto(x=230,y=180)
+end_line.right(90)
+end_line.pensize(10)
+end_line.color("red")
+end_line.pendown()
+end_line.forward(350)
+
 
 is_race_on = False
 screen = Screen()
 screen.setup(width=500,height=400)
 colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
-user_bet = screen.textinput(title="Make your bet",prompt="Which turtle will win the race? Enter color : ")
+user_bet = screen.textinput(title="Make your bet",prompt=f"Which turtle will win the race? \n{colors}\nEnter color : ")
 print(user_bet)
 x = -230
 y = -120
@@ -26,13 +38,16 @@ for color in colors:
 
 if user_bet:
     is_race_on = True
+
 while is_race_on:
     for turtle in turtle_list:
+        if int(turtle.xcor()) > 210:
+            is_race_on = False
+            race_winner(turtle)
+            break
+        
         random_distance = random.randint(0, 10)
         turtle.forward(random_distance)
-        if int(turtle.xcor()) == 100:
-            race_winner(turtle.color()[0])
-            is_race_on = False
 
 
 
