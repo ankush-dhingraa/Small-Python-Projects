d9490238c6b2ec12beda54162ae2337ffad578b0
ankush <ankushkumar20024@gmail.com>
2025-04-14 16:46:45 +0530

    turtle race game is ready to play

--- turtle-crossing-start/main.py
+++ turtle-crossing-start/main.py
@@ -11,6 +11,7 @@ screen.tracer(0)
 
 player = Player()
 car = CarManager()
+score = Scoreboard()
 
 screen.listen()
 screen.onkey(key="Up",fun=player.move_up)
@@ -29,8 +30,11 @@ while game_is_on:
     for cars in car.all_cars:
         if cars.distance(player) < 20:
             game_is_on = False
+            score.game_over()
     #detect if player cross other side
     if player.ycor() > 295:
+        score.score +=1
+        score.update_scoreboard()
         player.starting_position()
         car.level_up()
 
--- turtle-crossing-start/scoreboard.py
+++ turtle-crossing-start/scoreboard.py
@@ -1,5 +1,21 @@
-FONT = ("Courier", 24, "normal")
+from turtle import Turtle
+FONT = ("Courier", 18, "normal")
 
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("black")
+        self.hideturtle()
+        self.update_scoreboard()
 
-class Scoreboard:
-    pass
+    def update_scoreboard(self):
+        self.clear()
+        self.penup()
+        self.goto(-250,260)
+        self.write(f"Level : {self.score}",align="left", font=FONT)
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write(f"GAME OVER :(",align="center", font=FONT)
+
