0ef05e3cabfd112d94abb2443131fa3cd8143e86
ankush <ankushkumar20024@gmail.com>
2025-04-16 21:12:13 +0530

    Mail Merge project is ready

--- Mail Merge Project/main.py
+++ Mail Merge Project/main.py
@@ -5,19 +5,21 @@ with open(r"Mail Merge Project\mail_Input\Names\invited_names.txt") as name_file
         line = name_file.readline()
         if not line:
             break
-        name_list.append(line.strip())
-
+        if line != "\n":
+            name_list.append(line.strip())
 #read letter 
 with open(r"Mail Merge Project\mail_Input\Letters\starting_letter.txt") as letter_file:
     letter_list = letter_file.readlines()
-
+#create function to write new letters 
 def write_letter(name):
     letter = letter_list.copy()
     letter[0] = letter[0].replace('[name]',name)
     print(letter)
-    new_letter_path = r"Mail Merge Project\mail_Output\ReadyToSend\lettr_for_{0}".format(name)
+    new_letter_path = r"Mail Merge Project\mail_Output\ReadyToSend\letter_for_{0}".format(name)
     with open(new_letter_path,"w") as new_letter:
         for line in letter:
             new_letter.write(line)
 
-write_letter("lavisha")
+for name in name_list:
+    write_letter(name)
+print(name_list)
