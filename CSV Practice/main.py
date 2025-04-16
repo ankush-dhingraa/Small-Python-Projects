68ebd5bb2853c6d146d71d945b3822c169a5318e
ankush <ankushkumar20024@gmail.com>
2025-04-17 00:14:13 +0530

    start CSV practice

--- CSV Practice/main.py
+++ CSV Practice/main.py
@@ -1,4 +1,10 @@
 import csv
-with open("weather_data.csv") as csv_file:
+with open("CSV Practice\weather_data.csv") as csv_file:
     data = csv.reader(csv_file)
-print(data)
+    temperatures = []
+    for row in data:
+        temp = row[1].isnumeric()
+        if temp:
+            temperatures.append(int(row[1]))
+        
+print(temperatures)
