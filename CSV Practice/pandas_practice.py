c8f8200ea22dadaffc31769ae6176db2ded59670
ankush <ankushkumar20024@gmail.com>
2025-04-17 00:52:40 +0530

    start practice panda library

--- CSV Practice/pandas_practice.py
+++ CSV Practice/pandas_practice.py
@@ -6,3 +6,12 @@ data = pandas.read_csv("CSV Practice\weather_data.csv")
 
 #display specific column 'temp'
 print(data['condition'])
+
+#csv to dict
+data_dict = data.to_dict()
+print(data_dict)
+
+#series 1 dim and dataframe dim 2
+# create a list from csv series
+data_list = data["temp"].to_list()
+print(data_list)
