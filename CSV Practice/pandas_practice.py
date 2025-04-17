23b5342d339553fcbfca1d5bda5ad1b7cecea679
ankush <ankushkumar20024@gmail.com>
2025-04-17 23:10:51 +0530

    start practice panda library

--- CSV Practice/pandas_practice.py
+++ CSV Practice/pandas_practice.py
@@ -15,3 +15,11 @@ print(data_dict)
 # create a list from csv series
 data_list = data["temp"].to_list()
 print(data_list)
+print(data_dict)
+# print(data["day"][7]
+#method 1
+temperature_list = data["temp"].to_list()
+print(f"Average per day temperature is {round(sum(temperature_list)/len(temperature_list),2)}")
+#method 2
+print(f"Average per day temperature is {data["temp"].mean()}")
+print(f"Maximum temperature is : {data['temp'].max()}")
