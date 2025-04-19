47467c1168e650d6b2ed296480b16e188beed7eb
ankush <ankushkumar20024@gmail.com>
2025-04-19 23:41:47 +0530

    Start practicing with pandas library

--- The Great Squirrel Census Data Analysis with Pandas/main_copy.py
+++ The Great Squirrel Census Data Analysis with Pandas/main_copy.py
@@ -1,10 +1,13 @@
 import pandas
 data = pandas.read_csv(r"The Great Squirrel Census Data Analysis with Pandas\2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
-unique_color = data['Primary Fur Color'].unique()
-# print(unique_color)
-count_dict = {}
-for color in unique_color[1:]:
-    count_dict[color] = data[data['Primary Fur Color']==color].shape[0]
-new_data = pandas.DataFrame(list(count_dict.items()),columns=["Fur Colour","Total Count"])
+grey_squirrels = len(data[data['Primary Fur Color']=='Gray'])
+red_squirrels = len(data[data['Primary Fur Color']=='Cinnamon'])
+black_squirrels = len(data[data['Primary Fur Color']=='Black'])
+dict = {
+    'Fur color' : ['Grey', 'Cinnamon', 'Black'],
+    'Count' : [grey_squirrels,red_squirrels,black_squirrels]
+}
+
+new_data = pandas.DataFrame(dict)
 new_data.to_csv(r"The Great Squirrel Census Data Analysis with Pandas\Squirrel count by color copy.csv", index=False)
 # print(count_dict)
