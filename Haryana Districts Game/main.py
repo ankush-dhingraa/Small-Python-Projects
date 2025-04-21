5626304d287cee5d0e42e0298541c640c8834b17
ankush <ankushkumar20024@gmail.com>
2025-04-21 23:43:32 +0530

    start haryana districts game

--- Haryana Districts Game/main.py
+++ Haryana Districts Game/main.py
@@ -2,8 +2,8 @@ from turtle import Turtle,Screen
 obj = Turtle()
 screen = Screen()
 screen.screensize(300,300)
-screen.bgpic("India States Game\haryana new.gif")
-obj.goto(290,0)
+screen.bgpic("Haryana Districts Game\haryana.gif")
+obj.goto(16,135)
 
 
 
