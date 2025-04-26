9c8a9b2c548db65555f486fbd1afc45cf9447822
Ankush Dhingra <ankushkumar20024@gmail.com>
2025-04-27 00:00:52 +0530

    work in progress

--- NATO alphabet/main.py
+++ NATO alphabet/main.py
@@ -1,1 +1,13 @@
 import pandas
+data = pandas.read_csv(r"NATO alphabet\nato_phonetic_alphabet.csv")
+data_dict = {row.letter : row.code for (index,row) in data.iterrows()}
+# print(data)
+# print(data_dict)
+NATO = []
+user_input = input("Enter Word : ")
+for i in user_input.upper().split():
+    for key,value in data_dict.items():
+        if i == key:
+            NATO.append(value)
+
+print(NATO)
