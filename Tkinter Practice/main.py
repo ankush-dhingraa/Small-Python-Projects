68437408a4fc9a4198a9a309ad8fd12fe54f322b
Ankush Dhingra <ankushkumar20024@gmail.com>
2025-04-27 20:51:19 +0530

    work in progress

--- Tkinter Practice/main.py
+++ Tkinter Practice/main.py
@@ -2,7 +2,8 @@ import tkinter as tk
 
 window = tk.Tk()
 window.title("This is My Tkinter window")
-window.geometry("400x300")
+# window.geometry("400x300")
+window.minsize(600,300)
 
 
 window.mainloop()
