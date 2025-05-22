17314c83eca66254c83b0716264f3afe1fa3bc21
Ankush Dhingra <ankushkumar20024@gmail.com>
2025-05-22 20:12:29 +0530

    work in progress

--- PostgreSql/main.py
+++ PostgreSql/main.py
@@ -5,5 +5,17 @@ conn = psycopg2.connect(
     password = "1234",
     dbname = "school"
 )
+
+def insert(table,value,columns):
+    v = tuple(value)
+    cl = tuple(columns)
+    query = f"INSERT INTO {table} {columns} VALUES {v}"
+    return query
+
+
+cursor = conn.cursor()
+# cursor.execute(insert("student_details",columns=(name,'phone','class'),value=))
+cursor.execute("INSERT INTO student_details (name,phone,class) VALUES ('daksh',2589631475,'BCA')")
+conn.commit()
 conn.close()
 
