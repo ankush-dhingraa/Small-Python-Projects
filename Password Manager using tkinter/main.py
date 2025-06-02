558fefc6d94a4ee804b5c93a13bc38e30c711cc4
Ankush Dhingra <ankushkumar20024@gmail.com>
2025-06-03 00:15:30 +0530

    work in progress

--- Password Manager using tkinter/main.py
+++ Password Manager using tkinter/main.py
@@ -12,23 +12,35 @@ window.config(padx=20,pady=20)
 password_image = PhotoImage(file=r"Password Manager using tkinter\logo.png")
 canvas = Canvas(width=200,height=200)
 canvas.create_image(100,100,image=password_image)
-canvas.grid(column=0,row=2)
+canvas.grid(column=1,row=0)
 #Labels
 #website
 website_label = Label(text="website: ")
+website_label.grid(column=0,row=1)
 #email and username
 email_username_label = Label(text="Email\\Username: ")
+email_username_label.grid(column=0,row=2)
 #password
 password_label = Label(text="Password: ")
+password_label.grid(column=0,row=3)
 
 #entry input 
 website_input = Entry(width=50)
-website_input.grid(column=1,row=1)
+website_input.grid(column=1,row=1,columnspan=2)
 
 email_username_input = Entry(width=50)
+email_username_input.grid(column=1,row=2,columnspan=2)
 
 
 password_input = Entry(width=50)
+password_input.grid(column=1,row=3,columnspan=2)
+
+#buttons
+
+add_button = Button(text="Add",width=50,command="")
+add_button.grid
+
+generate_button = Button(text="Generate",width=20,command="")
 
 
 
