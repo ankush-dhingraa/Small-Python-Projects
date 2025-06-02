433909a50d62d9a9a94982cb4780421aa0d9f927
Ankush Dhingra <ankushkumar20024@gmail.com>
2025-06-03 00:06:43 +0530

    work on password manager

---
+++ Password Manager using tkinter/main.py
@@ -0,0 +1,29 @@
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+from tkinter import *
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20,pady=20)
+# window.minsize(width=500,height=500)
+#image canvas
+password_image = PhotoImage(file=r"Password Manager using tkinter\logo.png")
+canvas = Canvas(width=200,height=200)
+canvas.create_image(100,100,image=password_image)
+canvas.grid(column=0,row=2)
+#Labels
+#website
+website_label = Label(text="website: ")
+#email and username
+email_username_label = Label(text="Email\\Username: ")
+#password
+password_label = Label(text="Password: ")
+
+#entry input 
+
+
+
+
+window.mainloop()
